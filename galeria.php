dffa54a40dd7f9588ee3b811dab95762344f054b
Tomek <tomasz.szczygielski@hotmail.com>
2017-10-12 17:30:52 +0200

    Lab1 - wprowadzenie do PHP

---
+++ galeria.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Galeria</title>
+</head>
+<body>
+<?php
+galeria(4,2);
+
+function galeria($rows, $cols){
+    for($i = 1 ; $i<=$rows*$cols ; $i++){
+        print("<img src='miniaturki/obraz$i.JPG' alt='obraz$i' />");
+        if ($i%$cols === 0)
+            print ("</br>");
+    }
+}
+?>
--- index.php
+++ index.php
@@ -5,17 +5,8 @@
     <title>Title</title>
 </head>
 <body>
-<?php
-    galeria(4,2);
-
-    function galeria($rows, $cols){
-        for($i = 1 ; $i<=$rows*$cols ; $i++){
-            print("<img src='miniaturki/obraz$i.JPG' alt='obraz$i' />");
-            if ($i%$cols === 0)
-                print ("</br>");
-        }
-    }
-?>
-
+<h3>
+    <a href="galeria.php"> galeria</a>
+</h3>
 </body>
 </html>
