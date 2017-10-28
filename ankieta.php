72d0a779ef18ec4307c6afbfb28a97ddecd51307
Tomek <tomasz.szczygielski@hotmail.com>
2017-10-28 17:37:38 +0200

    zajecia 3 done

--- ankieta.php
+++ ankieta.php
@@ -5,3 +5,71 @@
  * Date: 23.10.17
  * Time: 10:25
  */
+
+$method = $_SERVER['REQUEST_METHOD'];
+define("POST", "POST");
+define("GET", "GET");
+define("ANKIETA_FILENAME", "ankieta.txt");
+
+$tech = ["C", "CPP", "Java", "C#", "HTML", "CSS", "XML", "PHP", "JavaScript"];
+
+
+if (!file_exists(ANKIETA_FILENAME) || filesize(ANKIETA_FILENAME) === 0) przygotuj_plik();
+if ($method === GET) {
+    przygotuj_form();
+}
+if ($method === POST) {
+    if (isset($_POST["ankieta"])) {
+        dodaj_wyniki();
+    }
+    wyswietl_wyniki();
+}
+
+function przygotuj_plik()
+{
+    $plik = fopen('ankieta.txt', 'w');
+    foreach ($GLOBALS['tech'] as $single_tech) {
+        fwrite($plik, $single_tech . " - 0" . PHP_EOL);
+    }
+}
+
+function wyswietl_wyniki()
+{
+    $plik = fopen(ANKIETA_FILENAME, 'r');
+    while (!feof($plik)) echo fgets($plik) . '<br/>';
+    fclose($plik);
+}
+
+function przygotuj_form()
+{
+    echo '<form name="ankieta_form" method="post">';
+    foreach ($GLOBALS['tech'] as $single_tech) {
+        echo $single_tech . "<input type='checkbox' name='$single_tech'><br/>";
+    }
+    echo '<input type="submit" name="ankieta">';
+    echo '</form>';
+}
+
+function dodaj_wyniki()
+{
+    $file = fopen(ANKIETA_FILENAME, 'r+');
+    $new_text = "";
+
+    if (!$file) {
+        echo 'problem z otwarciem pliku';
+        return -1;
+    }
+    while (!feof($file)) {
+        $line = fgets($file);
+        $line_array = explode(' ', $line);
+        if ($_POST[$line_array[0]]) {
+            $line_array[2] = $line_array[2] + 1;
+            $line = implode(' ', $line_array) . PHP_EOL;
+        }
+        $new_text .= $line;
+    }
+    rewind($file);
+    fputs($file, $new_text);
+    fclose($file);
+}
+
--- formularze.php
+++ formularze.php
@@ -9,37 +9,21 @@ include_once "formularz.html";
 // funkcje pomocnicze
 function przygotuj_dane()
 {
-    global $dane;
-    if (isset($_REQUEST['nazw']) && ($_REQUEST['nazw'] != "")) {
-        $nazwisko = htmlspecialchars(trim($_REQUEST['nazw']));
-    } else $nazwisko = 'x';
-    if (isset($_REQUEST['wiek']) && ($_REQUEST['wiek'] != "")) {
-        $wiek = htmlspecialchars(trim($_REQUEST['wiek']));
-    } else $wiek = 0;
-    if (isset($_REQUEST['panstwo']) && ($_REQUEST['panstwo'] != "")) {
-        $panstwo = htmlspecialchars(trim($_REQUEST['panstwo']));
-    } else $panstwo = 'x';
-    if (isset($_REQUEST['email']) && ($_REQUEST['email'] != "")) {
-        $email = htmlspecialchars(trim($_REQUEST['email']));
-    } else $email = 'x';
-    if (isset($_REQUEST['tutorial']) && ($_REQUEST['tutorial'] != "")) {
-        $tutorials = $_REQUEST['tutorial'];
-        $tutorials_stringified = '';
-        foreach ($tutorials as $tutorial) {
-            $tutorials_stringified = $tutorials_stringified . $tutorial . ',';
-        }
-    } else $tutorials_stringified = 'x';
-    if (isset($_REQUEST['zaplata'])) {
-        $zaplata = $_REQUEST['zaplata'];
-    } else $zaplata = 'x';
-
-    $dane =
-        $nazwisko . ' '
-        . $wiek . '   '
-        . $panstwo . '    '
-        . $email . '   '
-        . $tutorials_stringified . '   '
-        . $zaplata . "<br/>" . PHP_EOL;
+    $filter = array(
+        'nazwisko' => array('filter' => FILTER_VALIDATE_REGEXP,
+            'options' => array('regexp' => '/[A-Z]{1}[a-z]{2,15}/')),
+        'wiek' => array('filter' => FILTER_VALIDATE_INT,
+            'options' => array(
+                'min_range' => 13,
+                'max_range' => 90)),
+        'panstwo' => FILTER_DEFAULT,
+        'email' => FILTER_VALIDATE_EMAIL,
+        'tutorial' => array('filter' => FILTER_DEFAULT,
+            'flags' => FILTER_REQUIRE_ARRAY,
+        ),
+        'zaplata' => FILTER_DEFAULT
+    );
+    return filter_input_array(INPUT_GET, $filter);
 }
 
 function drukuj_form()
@@ -58,15 +42,63 @@ function pokaz()
 function dodaj()
 {
     $plik = fopen('plik.txt', 'a+');
-    przygotuj_dane();
-    if ($plik) {
-        echo "ok";
-        fwrite($plik, $GLOBALS['dane']);
-        fclose($plik);
+    $dane = przygotuj_dane();
+    if ($plik && !poinformuj_o_bledach($dane)) {
+        przygotuj_i_zapisz($dane,$plik);
     } else echo "problemy z otwarciem pliku spróbuj później";
+    fclose($plik);
+}
+ function poinformuj_o_bledach ($dane) : bool {
+    $czy_sa_bledy = false ;
+
+    if(empty($dane['nazwisko'])){
+        echo "Nazwisko było błędne lub puste!<br/>";
+        $czy_sa_bledy = true;
+    }
+    if(empty($dane['wiek'])){
+        echo "Wiek musi być z zakresu 13 do 90!<br/>";
+        $czy_sa_bledy = true;
+    }
+    if(empty($dane['panstwo'])){
+        echo "Wybrane państwo jest błędne!<br/>";
+        $czy_sa_bledy = true;
+    }
+    if(empty($dane['email'])){
+        echo "Email był błędny lub pusty!<br/>";
+        $czy_sa_bledy = true;
+    }
+    if(empty($dane['tutorial'])){
+        echo "Nie wybrano żadnego z tutoriali!<br/>";
+        $czy_sa_bledy = true;
+    }
+    if(empty($dane['zaplata'])){
+        echo "Nie wybrano sposobu zapłaty!<br/>";
+        $czy_sa_bledy = true;
+    }
+    return $czy_sa_bledy;
+}
+function przygotuj_i_zapisz($dane,$plik){
+    $tutorials = $dane['tutorial'];
+    $tutorials_stringified = '';
+    foreach ($tutorials as $tutorial) {
+        $tutorials_stringified = $tutorials_stringified . $tutorial . ',';
+    };
+    $dane_string = $dane['nazwisko'].' '.
+        $dane['wiek'].' '.
+        $dane['panstwo'].' '.
+        $dane['email'].' '.
+        $tutorials_stringified.' '.
+        $dane['zaplata'].PHP_EOL;
+    fwrite($plik,$dane_string);
+}
+function pokaz_jezyk(string $language_nename)
+{
+    if ($file = fopen("plik.txt", "r")) {
+        find_and_echo($language_nename, $file);
+        fclose($file);
+    } else echo "file not opened";
 
 }
-
 function find_and_echo(string $searched, $handle)
 {
 
@@ -76,16 +108,7 @@ function find_and_echo(string $searched, $handle)
     }
 }
 
-function pokaz_jezyk(string $language_nename)
-{
-    if ($file = fopen("plik.txt", "r")) {
-        find_and_echo($language_nename, $file);
-        fclose($file);
-    } else echo "file not opened";
-
-}
-
-drukuj_form();
+//przygotuj_plik();
 if (isset($_REQUEST['pokaz'])) {
     pokaz();
 } else if (isset($_REQUEST['dodaj'])) {
@@ -96,4 +119,59 @@ if (isset($_REQUEST['pokaz'])) {
     pokaz_jezyk("PHP");
 } else if (isset($_REQUEST['pokazcpp'])) {
     pokaz_jezyk("C\C++");
+} else if (isset($_REQUEST['statystyki'])) {
+    pokaz_statystyki();
+}
+
+
+function policz_linie()
+{
+    $ilosc_wpisow = 0;
+    if ($file = fopen("plik.txt", "r")) {
+        while (!feof($file)) {
+            $line = fgets($file);
+            $ilosc_wpisow++;
+        }
+        fclose($file);
+    } else echo "file not opened";
+    return $ilosc_wpisow;
+}
+
+function policz_ponizej_18()
+{
+    $ilosc_wpisow = 0;
+    if ($file = fopen("plik.txt", "r")) {
+        while (!feof($file)) {
+            $line = fgets($file);
+            $line_array = explode(" ", $line);
+            if ($line_array[2] < 18) {
+                $ilosc_wpisow++;
+            }
+        }
+        fclose($file);
+    } else echo "file not opened";
+    return $ilosc_wpisow;
+}
+
+function policz_powyzej_50()
+{
+    $ilosc_wpisow = 0;
+    if ($file = fopen("plik.txt", "r")) {
+        while (!feof($file)) {
+            $line = fgets($file);
+            $line_array = explode(";", $line);
+            if ($line_array[1] > 50) {
+                $ilosc_wpisow++;
+            }
+        }
+        fclose($file);
+    } else echo "file not opened";
+    return $ilosc_wpisow;
+}
+
+function pokaz_statystyki()
+{
+    echo 'ilosc wszystkich wpisow wynosi ' . policz_linie() . '<br/>';
+    echo 'ilość wpisów od osób poniżej 18 roku życia: ' . policz_ponizej_18() . '<br/>';
+    echo 'ilość wpisów od osób powyżej 50 roku życia: ' . policz_powyzej_50() . '<br/>';
 }
